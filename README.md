b4fe8ef609da3059983bac632755ef78ad7db171
nguyendm77 <nguyendm77@gmail.com>
2020-11-20 16:07:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# [Translation]()
+
+
+## [Screenshot]()
+
+
+### Usage: 
+
+1. This app using react-i18next documentation. For referencing, you can find it at [here](https://react.i18next.com/).
+
+2. Type  " npm install " in the root directory  ( Download Front-end Dependencies )
+
+3. Type " npm run start " to run this application
+
+
+
+
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+
+function App() {
+  const { t } = useTranslation();
+
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
+  return (
+    <div className="App">
+      <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+        <button onClick={() => handleClick('en')} >
+          English
+          </button>
+        <button onClick={() => handleClick('spn')} >
+          Spanish
+          </button>
+        <button onClick={() => handleClick('chi')} >
+          Chinese
+          </button>
+        <button onClick={() => handleClick('vn')} >
+          Vietnamese
+          </button>
+      </nav>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h3>{t('Hi.1')}</h3>  <h3>{t('Where.1')}</h3> <h3>{t('From.1')}</h3>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
